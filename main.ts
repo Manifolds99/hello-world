20bbcc1369798e46d01444c5a9a96969ea413eca
Manifolds99 <122555271+Manifolds99@users.noreply.github.com>
2023-01-13 12:07:48 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Square)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.SmallSquare)
+})
+basic.showIcon(IconNames.Heart)
